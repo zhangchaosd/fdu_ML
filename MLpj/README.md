90a0a2777b0287703af1f12d9104076c4e31ae82
zhangchao <zhangchaogoo@gmail.com>
2021-11-23 13:28:12 +0800

    update

---
+++ MLpj/README.md
@@ -0,0 +1,18 @@
+## 任务1：定性分析（布眼数据集）
+### 1.1 混合材质纺织物，由纯棉（Cotton）和棉混纺（Cotton+X）组成 本任务为二分类
+### 1.2 关于数据集
+老师给的"布眼数据集.json"有一条数据有问题需要去掉，去掉之后的"布眼数据集fixed.json"共有70026条数据，下载地址为 https://drive.google.com/file/d/1SBX_lIny3KfxX2JOaePPnQWNBavSvGs5/view?usp=sharing
+
+一条数据的格式
+<img src="assets/onerecord.png" >
+
+### 1.3 评测指标：Accuracy，目前最好能到 90%
+### 1.4 运行 pj2.py
+先设置文件开头的 DATAPATH 变量。
+每次运行随机取10000条作为验证集，10000条作为测试集，剩下的50026条作为训练集
+
+## 任务2：定量分析
+### 2.1 混合材质纺织物，由棉（Cotton）和聚酯纤维（Poly）组成 本次任务为预测织物中Poly的含量
+### 2.2 评测指标：MAE（平均绝对值误差），目前最好能到 0.075
+### 2.3 运行 pj3.py
+先设置文件开头的 DATAPATH 变量。
--- MLpj/pj2.py
+++ MLpj/pj2.py
@@ -8,12 +8,13 @@ from torch.utils.data import DataLoader
 
 #DATAPATH = 'D:/9709/Desktop/works/fdu_ML/MLpj/布眼数据集.json'
 DATAPATH = 'C:/Users/97090/Desktop/fdu_ML/MLpj/布眼数据集fixed.json'
+# 布眼数据集fixed.json: https://drive.google.com/file/d/1SBX_lIny3KfxX2JOaePPnQWNBavSvGs5/view?usp=sharing
 BATCHSIZE = 512
-EPOCH = 100
+EPOCH = 500
 
 class Dataset2(torch.utils.data.dataset.Dataset):
     def __init__(self, jsf, train = True, val = False):
-        self.jsf = jsf # 70027 70026
+        self.jsf = jsf # 70027 fixed:70026
         self.testData = self.jsf[:10000]
         self.valData = self.jsf[10000:20000]
         self.trainData = self.jsf[20000:]
@@ -99,7 +100,11 @@ class Net22(nn.Module): #33epoch 8528 512 0.00001
             nn.BatchNorm1d(num_features = in_feature),
             nn.Linear(in_feature, hidden_dim),
             nn.LeakyReLU(),
-            nn.Linear(hidden_dim, 2),
+            nn.Linear(4096, 2048),
+            nn.LeakyReLU(),
+            nn.Linear(2048, 1024),
+            nn.LeakyReLU(),
+            nn.Linear(1024, 2),
             nn.Softmax(dim = 1)
         )
 
@@ -107,7 +112,7 @@ class Net22(nn.Module): #33epoch 8528 512 0.00001
         return self.fc2(x)
 
 class Net23(nn.Module):
-    def __init__(self, in_feature = 912 * 2, hidden_dim = 4096) -> None:
+    def __init__(self, hidden_dim = 4096) -> None:
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv1d(in_channels = 1, out_channels=1, kernel_size=11, padding = 5),
@@ -132,7 +137,10 @@ class Net23(nn.Module):
             nn.Conv1d(in_channels = 1, out_channels=1, kernel_size=1),
         )
         self.fc = nn.Sequential(
-            nn.Linear(in_features=57,out_features=2),
+            nn.Linear(in_features=57,out_features=32),
+            nn.LeakyReLU(),
+            nn.Linear(32, 2),
+            nn.LeakyReLU(),
             nn.Softmax(dim=1)
         )
 
@@ -142,8 +150,7 @@ class Net23(nn.Module):
         out = torch.squeeze(input=out,dim=1)
         return self.fc(out)
 
-def mission2():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+def mission2(model, batchsize, lr, device):
     print("Using {} device".format(device))
     print('mission 2 start')
     jsf = json.load(open(DATAPATH))
@@ -151,13 +158,15 @@ def mission2():
     training_data = Dataset2(jsf = jsf, train = True)
     val_data = Dataset2(jsf = jsf, train = False, val = True)
     # test_data = Dataset2(jsf = jsf, train = False, val = False)
-    train_dataloader = DataLoader(training_data, batch_size = BATCHSIZE, shuffle = True)
+    train_dataloader = DataLoader(training_data, batch_size = batchsize, shuffle = True)
     val_dataloader = DataLoader(val_data, batch_size = 10, shuffle = True)
     # test_dataloader = DataLoader(test_data, batch_size = 1000, shuffle = True)
 
-    model = Net23().to(device)
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr = 0.00003)
+    model = Net22().to(device)
+    weight=torch.tensor([0.28, 0.72]).to(device)
+    loss_fn = nn.CrossEntropyLoss(weight = weight)
+    optimizer = torch.optim.AdamW(model.parameters(), lr = lr)
+    tmax=0
     for i in range(EPOCH):
         model.train()
         for batch, (x, x2, y) in enumerate(train_dataloader):
@@ -197,13 +206,25 @@ def mission2():
                 else:
                     CXtotal += 1
         acc = correct / total
-        print('EPOCH: ', i,'total: ', correct, total, 'C: ', C, Ctotal,'CX: ', CX, CXtotal)
-        if i % 20 == 0:
-                print('update lr, saved model')
-                for param_group in optimizer.param_groups:
-                    param_group['lr'] *= 0.3
+        if acc>tmax:
+            tmax=acc
+        Cacc = C / Ctotal
+        CXacc = CX / CXtotal
+        print('EPOCH: ', i,'total: ', acc, 'C: ', Cacc, 'CX: ', CXacc)
+        if (i+1) % 7 == 0 or C == 0 or CX == 0:
+            print('update lr')
+            for param_group in optimizer.param_groups:
+                param_group['lr'] *= 0.3
+        if acc < 0.8 and C !=0 and CX != 0:
+            print('add lr')
+            for param_group in optimizer.param_groups:
+                param_group['lr'] *= 3
+    print('tmax: ', tmax)
 
 if __name__ == '__main__':
     #ck()
-    mission2()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    #mission2(model = Net22().to(device), batchsize=512, device=device, lr = 0.00003) #23 0.8972 0.3 0.7
+    mission2(model = Net23().to(device), batchsize=1024, device=device, lr = 0.0000003)
+
     exit()
