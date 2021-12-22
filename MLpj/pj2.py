63c1b22d54ecd7fc6d9d2d5acf4aa91989573725
“zhangchao” <zhangchaogoo@gmail.com>
2021-12-22 13:05:54 +0800

    update

--- MLpj/pj2.py
+++ MLpj/pj2.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 from torch.nn.modules import normalization
 from torch.nn.modules.activation import Softmax
+from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
 import os
 
@@ -15,29 +16,41 @@ BATCHSIZE = 512
 EPOCH = 70
 
 class Dataset2(torch.utils.data.dataset.Dataset):
-    def __init__(self, data, train = True, val = False):
-        # self.testData = data[:10000]
-        # self.valData = data[10000:20000]
-        # self.trainData = data[20000:]
-        self.valData = data[:10000]
-        self.trainData = data[10000:]
-        self.train = train
-        self.val = val
+    def __init__(self, dic, ids, isTrain = True):
+        np.random.shuffle(ids)
+        self.dic = dic
+        num_total = len(ids)
+        num_train = int(num_total * 0.9)
+        self.train_ids = ids[:num_train]
+        self.val_ids = ids[num_train:]
+        self.train = []
+        self.val = []
+        for id in self.train_ids:
+            self.train = self.train + dic[id]
+        for id in self.val_ids:
+            self.val = self.val + dic[id]
+        self.isTrain = isTrain
 
     def __len__(self):
-        if self.train:
-            return len(self.trainData)
-        if self.val:
-            return len(self.valData)
-        return len(self.testData)
+        if self.isTrain:
+            return len(self.train)
+        return len(self.val_ids)
 
     def __getitem__(self, idx):
-        data = self.trainData[idx] if self.train else self.valData[idx] if self.val else self.testData[idx]
-        label = int(data[-1])
-        x = data[:-1]
-        oh = [0., 0.]
-        oh[label] = 1.
-        return x, torch.tensor(oh)
+        if self.isTrain:
+            data = self.train[idx]
+            label = int(data[-1])
+            x = data[:-1]
+            oh = [0., 0.]
+            oh[label] = 1.
+            return x, torch.tensor(oh)
+        else:
+            x = []
+            id = self.val_ids[idx]
+            datas = self.dic[id]
+            for data in datas:
+                x = x + data
+        return x, torch.tensor([0., 0.])
 
 '''
 fc 1824 4096
@@ -178,6 +191,73 @@ def mission2(model, data, batchsize, lr, device, addfactor = 3): #data.shape = 7
     print('tmax: ', tmax)
     return tmax
 
+
+def mission25(model, dic, ids, batchsize, lr, device, addfactor = 3): #data.shape = 70026, 1825
+    print("Using {} device".format(device))
+    print('mission 2 start')
+    training_data = Dataset2(dic = dic,ids = ids, train = True)
+    val_data = Dataset2(dic = dic,ids = ids, train = False, val = True)
+    # test_data = Dataset2(data=data, train = False, val = False)
+    train_dataloader = DataLoader(training_data, batch_size = batchsize, shuffle = True)
+    val_dataloader = DataLoader(val_data, batch_size = 10, shuffle = True) # batchsize
+    # test_dataloader = DataLoader(test_data, batch_size = 1000, shuffle = True)
+
+    weight = torch.tensor([0.28, 0.72]).to(device) #965
+    loss_fn = nn.CrossEntropyLoss(weight = weight)
+    optimizer = torch.optim.AdamW(model.parameters(), lr = lr)
+    tmax = 0
+    for i in range(EPOCH):
+        model.train()
+        for _, (x, y) in enumerate(train_dataloader):
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            loss = loss_fn(pred, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # print('loss ', loss.item())
+        continue
+        # val
+        model.eval()
+        total = len(val_data)
+        correct = 0
+        Ctotal = 0
+        CXtotal = 0
+        C = 0
+        CX = 0
+        for _, (x, y) in enumerate(val_dataloader):
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            labs = torch.argmax(pred, dim = 1)
+            gt = torch.argmax(y, dim = 1)
+            for j in range(x.shape[0]):
+                if labs[j] == gt[j]:
+                    correct += 1
+                    if gt[j] == 1:
+                        C += 1
+                    else:
+                        CX += 1
+                if gt[j] == 1:
+                    Ctotal += 1
+                else:
+                    CXtotal += 1
+        acc = correct / total
+        if acc > tmax:
+            tmax = acc
+        Cacc = C / Ctotal
+        CXacc = CX / CXtotal
+        print('EPOCH:', i,'total:', acc, 'C:', round(Cacc, 2), 'CX:', round(CXacc, 2))
+        if (i + 1) % 7 == 0 or C == 0 or CX == 0:
+            print('update lr')
+            for param_group in optimizer.param_groups:
+                param_group['lr'] *= 0.3
+        if acc < 0.8 and C != 0 and CX != 0:
+            print('add lr')
+            for param_group in optimizer.param_groups:
+                param_group['lr'] *= addfactor
+    print('tmax: ', tmax)
+    return tmax
+
 def loadData(datapath = DATAPATH):
     print('Now loading data')
     cache_dir = os.getcwd() + '/data_fixed.npy'
@@ -216,14 +296,58 @@ def loadData(datapath = DATAPATH):
     print('cache saved')
     return torch.tensor(data)
 
+def standardization(data):
+    mu = torch.mean(data, axis=0)
+    sigma = torch.std(data, axis=0)
+    return (data - mu) / sigma
+
+def loadData2(datapath):
+    print('Now loading data')
+    cache_dir = os.getcwd() + '/data2_fixed.npy'
+    cacheids_dir = os.getcwd() + '/data2ids_fixed.npy'
+    print(cache_dir)
+    if os.path.exists(cache_dir):
+        print('use cache')
+        return np.load(cache_dir), np.load(cacheids_dir)
+    print('reading...')
+    jsf = json.load(open(datapath))
+    data = []
+    ids = []
+    dic = {}
+    for re in jsf:
+        label = 1 if len(re['l']) == 1 else 0
+        r = torch.tensor(re['r'])
+        rl = standardization(r)
+        a = torch.tensor(re['a'])
+        al = standardization(a)
+        i = torch.tensor(re['i'])
+        il = standardization(i)
+        l = torch.tensor([label])
+        id = re['id']
+        rail = torch.cat((rl,al,il), dim = 0)
+        rair = torch.cat((r,a,i), dim = 0)
+        rair = standardization(rair)
+        data = torch.cat((rail, rair, l), dim = 0)
+        if id in dic:
+            dic[id].append(data)
+        else:
+            dic[id] = [data]
+            ids.append(id)
+    np.save(cache_dir, dic)
+    np.save(cacheids_dir, ids)
+    print('cache saved')
+    return dic, ids
+
 if __name__ == '__main__':
     ret = []
+    datapath = os.getcwd() + '/MLpj/布眼数据集fixed.json'
     for i in range(10):
-        data = loadData(DATAPATH)
-        print(data.shape)
+        dic, ids = loadData2(datapath)
+        # print(data.shape)
         device = "cuda" if torch.cuda.is_available() else "cpu"
         # ret.append(mission2(model = Net2().to(device), data = data, batchsize = 128, device = device, lr = 0.00003, addfactor = 3)) # 96.5
-        ret.append(mission2(model = Net2().to(device), data = data, batchsize = 512, device = device, lr = 0.00003, addfactor = 3)) # 96.5
+        # ret.append(mission2(model = Net2().to(device), data = data, batchsize = 512, device = device, lr = 0.00003, addfactor = 3)) # 96.5
+        ret.append(mission25(model = Net2().to(device), dic = dic, ids = ids, batchsize = 512, device = device, lr = 0.00003, addfactor = 3)) # 96.5
     print('final ret:')
     print(ret)
     exit()
